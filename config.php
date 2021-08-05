b74b848138ef9017918dd71111e2c76bd3b17e47
romuloff23 <57837232+romuloff23@users.noreply.github.com>
2021-08-05 10:13:34 -0300

    credenciais

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
     //credenciais
     const URL = 'https://webmaniabr.com/api/';
-    const Consumer_Key = 'M4hDAfNkQijWdn8166O1qSd34QFlbrnO';
-    const Consumer_Secret = 'bAIg88WKWx8o6pesWHS6HTXGCFtxELsmmsgEHtWhIJYGwBma';
-    const Access_Token = '2011-aL6I0LvKVKrdsuqYXfyfy4QoDSpbCjkYBbH8YZUUUqp7Mk8D';
-    const Access_Token_Secret = 'isDDxhlLfDqBaKhxqD2TFVRiq2pjT517bUZZVMmsJEyZuYjZ';
+    const Consumer_Key = 'SUA KEY';
+    const Consumer_Secret = 'SUA SECRET';
+    const Access_Token = 'SEU TOKEN';
+    const Access_Token_Secret = 'SEU TOKE SECRET';
     //config DB
     const DBDRIVE = 'mysql';
     const DBHOST = 'localhost';
